2ab263408cd5d2022305aa1126b1bf1cbf41afe9
aarya-ghimire <aaryaghimire36@gmail.com>
2024-10-01 17:36:07 +0545

    initial commit

---
+++ src/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer>
+      <p>&copy; 2022 - My React App</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/Header.jsx
@@ -0,0 +1,22 @@
+function Header() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <nav>
+        <ul>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default Header;
